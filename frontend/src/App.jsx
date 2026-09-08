08565b02b1b8ad4f327067dc791914391e4092cc
Jamilly <ronijamilly.lima@sptech.school>
2026-09-07 22:20:45 -0300

    iniciando projeto vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Restaurante from "./componentes/Restaurante"
+
+function App() {
+  return(
+  <div>
+    <Restaurante/>
+  </div>
+  )
+}
+
+export default App
---
+++ frontend/src/componentes/Restaurante.jsx
@@ -0,0 +1,10 @@
+function Restaurante() {
+
+    return (
+        <div>
+            <h1>Teste</h1>
+        </div>
+    )
+}
+
+export default Restaurante;
